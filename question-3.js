ebf0e9bab950adc90cc0c43c35ee46147455264a
champ <champdrm@gmail.com>
2025-12-29 14:28:04 +0700

    feat: add answer q-3

--- question-3.js
+++ question-3.js
@@ -1,1 +1,21 @@
 // เริ่มเขียนโค้ดตรงนี้
+let getFirstUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    return data; 
+}
+
+async function Jsonplaceholder() {
+    try {
+        let getUsers = await getFirstUsers()
+        // console.log(getUsers);
+        let result = getUsers.filter((name) => name.name.length > 17)
+        .map(name => name.name)
+        console.log(result)
+    } catch (error) {
+        
+    }
+}
+
+
+Jsonplaceholder()
